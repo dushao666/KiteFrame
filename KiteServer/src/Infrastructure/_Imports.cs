9862cd5974650796f34727b9eb36d8e1aea11ad2
dushaoqi <13283061807@163.com>
2025-09-10 16:45:57 +0800

    1

--- KiteServer/src/Infrastructure/_Imports.cs
+++ KiteServer/src/Infrastructure/_Imports.cs
@@ -9,6 +9,7 @@ global using System.Threading;
 global using System.Threading.Tasks;
 global using System.Text;
 global using System.Text.Json;
+global using System.Linq.Expressions;
 
 // Microsoft Extensions
 global using Microsoft.Extensions.Configuration;
